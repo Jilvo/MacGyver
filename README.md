56710dfc913da78c1f8f4b2dc92b1e52e835c319
Jilvo <jmillot41@gmail.com>
2019-10-15 16:32:19 +0200

    git push -f

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MacGyver
 Project 3 from the OpenClassrooms's school
+Versionnage du projet:
+
+V0.1: début du projet et du labyrinthe
+V0.2: écriture papier du projet
+V0.4: labyrinthe fini et fonctionnel
+V0.5: affichage du labyrinthe dans Pygame
+V0.6: déplacement de macgyver sur Pygame et intération avec les objets
+V0.7: affichage du score et écran de fin
--- lab-pygame.py
+++ lab-pygame.py
@@ -1,4 +1,4 @@
-#V 0.1 pygame
+#V 0.5 pygame
 #import modules
 import pygame
 from pygame.locals import *
@@ -8,44 +8,63 @@ from main import *
 pygame.init()
 screen = pygame.display.set_mode((cote_screen, cote_screen))
 
-#Chargement et collage du fond
-fond = pygame.image.load("MacGyver/ressources/background.jpg").convert()
 
-#set the guard
-badguy = pygame.image.load(image_badguy).convert_alpha()
-x=list_badguy[0]*taille_sprite
-y=list_badguy[1]*taille_sprite
-#set the wall
-# wall= pygame.image.load(image_wall).convert_alpha
-
-# for y,lst in enumerate(structure_labyrinth):
-# 	for x,color in enumerate(lst):
-# 		if color =="*":
-# 			x=color*taille_sprite
-# 			y=lst*taille_sprite
-# 			screen.blint(wall,(x,y))
-# 	pygame.display.flip()
-
-#set the object
+# fond = pygame.image.load("MacGyver/ressources/background.jpg").convert()
+#set the objects and stuff
 ether= pygame.image.load(image_ether).convert_alpha()
 tube= pygame.image.load(image_tube).convert_alpha()
 needle= pygame.image.load(image_needle).convert_alpha()
 wall = pygame.image.load(image_wall).convert_alpha()
+macgyver = pygame.image.load(image_macgyver).convert_alpha()
+badguy = pygame.image.load(image_badguy).convert_alpha()
+
+x=list_badguy[0]*taille_sprite
+y=list_badguy[1]*taille_sprite
+# wall= pygame.image.load(image_wall).convert_alpha
+
+for y,lst in enumerate(structure_labyrinth):
+	for x,color in enumerate(lst):
+		if color =="*":
+			a=x*taille_sprite
+			b=y*taille_sprite
+			screen.blit(wall,(a,b))
+		if color =="b":
+			a=x*taille_sprite
+			b=y*taille_sprite
+			screen.blit(badguy,(a,b))
+		if color =="m":
+			a=x*taille_sprite
+			b=y*taille_sprite
+			position_perso = macgyver.get_rect()
+			position_perso.center=a,b
+			screen.blit(macgyver, position_perso)
+			# screen.blit(macgyver,(a,b))
+			pygame.display.flip()
+		if color =="n":
+			a=x*taille_sprite
+			b=y*taille_sprite
+			screen.blit(needle,(a,b))
+		if color =="t":
+			a=x*taille_sprite
+			b=y*taille_sprite
+			screen.blit(tube,(a,b))
+		if color =="e":
+			a=x*taille_sprite
+			b=y*taille_sprite
+			screen.blit(ether,(a,b))
+		pygame.display.flip()
+
+		
+
+#set the object
+
 #Chargement et collage du personnage
-perso = pygame.image.load("MacGyver/ressources/macgyver.png").convert_alpha()
-position_perso = perso.get_rect()
 
-screen.blit(perso, position_perso)
-Score=1
-#Rafraîchissement de l'écran
-# def display_labyrinth(structure_labyrinth):
-#     for y,lst in enumerate(structure_labyrinth):
-#         for x,color in enumerate(lst):
-#             if color == "*":
-# 			    screen.blit(wall,(structure_labyrinth[i]*taille_sprite,structure_labyrinth[i][j]*taille_sprite))
-# 		    else:
-# 		        pass	
 
+# def score_board(score,ether,needle,tube)
+# 	if score==0
+
+# 	if 
 
 continuer = 1
 while continuer:
@@ -55,28 +74,28 @@ while continuer:
 		if event.type == KEYDOWN:
 			if event.key == K_DOWN:	#Si "flèche bas"
 				#On descend le perso
+				deplacement.deplacer('down')
 				position_perso = position_perso.move(0,taille_sprite)
+				pygame.display.flip()
 		if event.type == KEYDOWN:
 			if event.key == K_UP:	#Si "flèche bas"
 				#On descend le perso
+				deplacement.deplacer('up')
 				position_perso = position_perso.move(0,-taille_sprite)
+				pygame.display.flip()
 		if event.type == KEYDOWN:
 			if event.key == K_RIGHT:	#Si "flèche bas"
 				#On descend le perso
+				deplacement.deplacer('right')
 				position_perso = position_perso.move(taille_sprite,0)
+				pygame.display.flip()
 		if event.type == KEYDOWN:
 			if event.key == K_LEFT:	#Si "flèche bas"
 				#On descend le perso
+				deplacement.deplacer('left')
 				position_perso = position_perso.move(-taille_sprite,0)
-		
-	
-	#Re-collage
-	screen.blit(fond, (0,0))	
-	screen.blit(badguy,(x,y))
-	screen.blit(perso, position_perso)
-	screen.blit(ether, (list_object[0][0]*taille_sprite,list_object[0][1]*taille_sprite))
-	screen.blit(needle, (list_object[1][0]*taille_sprite,list_object[1][1]*taille_sprite))
-	screen.blit(tube, (list_object[2][0]*taille_sprite,list_object[2][1]*taille_sprite))
-	
-	#Rafraichissement
+				pygame.display.flip()
+		pygame.display.flip()
+	screen.blit(macgyver, position_perso)
+	# screen.blit(macgyver, position_perso)
 	pygame.display.flip()
