31bd40eeecdbe593032f15a60552162ffa088624
Jilvo <jmillot41@gmail.com>
2019-11-14 15:30:10 +0100

    delete files

--- macGyver.py
+++
@@ -1,9 +0,0 @@
-#V0.1.2
-#import modules
-import json
-
-
-labyrinth=[['*' for i in range(15)],['*'] + ['v'] * 13 + ['*']];
-for i in range(13):
-    labyrinth.append(['*'] + ['v'] * 13 + ['*']);
-labyrinth.append(['*' for i in range(15)]);
